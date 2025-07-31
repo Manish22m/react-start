8c59664efacf68174493e2b8de0c056838f3327c
Manish Mengane <manishmengane10@gmail.com>
2025-07-31 14:32:43 +0530

    completed project on reduxtoolkit

---
+++ reductollkit/src/features/todo/todoSlice.js
@@ -0,0 +1,35 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState={
+    todos:[{id: 1, Text:'learner'}]
+}
+
+export const todoSlice=createSlice({
+    name:'todo',
+    initialState,
+    reducers:{
+        addtodo: (state, action) =>{
+            const todo={
+                id: nanoid(),
+                text:action.payload
+            }
+            state.todos.push(todo)
+        },
+
+        removetodo:(state, action) =>{
+            state.todos=state.todos.filter((todo)=>todo.id !== action.payload)
+        },
+
+        updatetodo: (state, action) => {
+        const { id, text } = action.payload;
+        const todoUpdate = state.todos.find(todo => todo.id === id);
+        if (todoUpdate) {
+        todoUpdate.Text = text;
+        }
+        }
+
+    }
+})
+export const{addtodo, removetodo,updatetodo}=todoSlice.actions
+
+export default todoSlice.reducer
